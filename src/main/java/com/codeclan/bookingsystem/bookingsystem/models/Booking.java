bdfcf6fa46f2172458ba68c5710222f65d3281cb
Mateusz <matstanczak@gmail.com>
2019-05-23 10:40:32 +0100

    BookingModelTest passing

--- src/main/java/com/codeclan/bookingsystem/bookingsystem/models/Booking.java
+++ src/main/java/com/codeclan/bookingsystem/bookingsystem/models/Booking.java
@@ -1,4 +1,17 @@
 package com.codeclan.bookingsystem.bookingsystem.models;
 
 public class Booking {
+    private String date;
+
+    public Booking(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
--- src/test/java/com/codeclan/bookingsystem/bookingsystem/modelsTest/BookingTest.java
+++ src/test/java/com/codeclan/bookingsystem/bookingsystem/modelsTest/BookingTest.java
@@ -1,4 +1,28 @@
 package com.codeclan.bookingsystem.bookingsystem.modelsTest;
 
+import com.codeclan.bookingsystem.bookingsystem.models.Booking;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookingTest {
+
+    Booking booking;
+
+    @Before
+    public void setUp() {
+        booking = new Booking("2019-05-23");
+    }
+
+    @Test
+    public void hasDate() {
+        assertEquals("2019-05-23", booking.getDate());
+    }
+
+    @Test
+    public void setDate() {
+        booking.setDate("2019-05-24");
+        assertEquals("2019-05-24", booking.getDate());
+    }
 }
